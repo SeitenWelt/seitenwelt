3d03a0228020efc76ef4fd8eff9c1c84816ae2dc
SeitenWelt <info@seitenwelt.de>
2025-04-13 00:17:57 +0200

    Update codes.js

--- codes.js
+++ codes.js
@@ -26,14 +26,13 @@ function isCodeValid(inputCode) {
 
   const diffDays = Math.floor((today - created) / (1000 * 60 * 60 * 24));
 
-
-
   return diffDays <= 240;
 
 }
 
 
 
-// Global verfügbar machen
+// Macht die Funktion im Browser global verfügbar
 
 window.isCodeValid = isCodeValid;
+
