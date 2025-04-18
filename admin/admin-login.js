45dd52c6e42dc5638cb1164f02457b5ac3ae600d
SeitenWelt <info@seitenwelt.de>
2025-04-18 18:01:36 +0200

    Update admin-login.js

--- admin/admin-login.js
+++ admin/admin-login.js
@@ -1,10 +1,12 @@
-function login() {
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
   const username = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value;
 
   if (username === "Admin2025" && password === "SeitenWelt!") {
     window.location.href = "dashboard.html";
   } else {
-    document.getElementById("loginMessage").innerText = "Zugangsdaten sind falsch.";
+    document.getElementById("error-message").textContent =
+      "Falscher Benutzername oder Passwort.";
   }
-}
+});
